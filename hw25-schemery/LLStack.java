13e71f9acacbe3fd3ff27ccf581c1bd987209c1b
Max Korsun <mkorsun@stuy.edu>
2017-03-27 08:56:55 -0400

    25

---
+++ hw25-schemery/LLStack.java
@@ -0,0 +1,111 @@
+/*****************************************************
+ * class LLStack
+ * Implements a stack, generically typed.
+ * Uses a LinkedList as underlying container.
+ *****************************************************/
+
+import java.util.LinkedList;
+
+public class LLStack<T> implements Stack<T> 
+{
+    private LinkedList<T> _stack;
+
+    //constructor
+    public LLStack() 
+    { 	
+	/* YOUR IMPLEMENTATION HERE*/
+    }
+
+
+    //means of insertion
+    public void push( T s ) 
+    {
+	/* YOUR IMPLEMENTATION HERE*/
+    }
+
+
+    //means of viewing top element without removing
+    public T peek( ) 
+    { 
+	/* YOUR IMPLEMENTATION HERE*/
+    }
+
+
+    //means of removal
+    public T pop( ) 
+    { 
+	/* YOUR IMPLEMENTATION HERE*/
+    }
+
+
+    //chk for emptiness
+    public boolean isEmpty() 
+    {
+	/* YOUR IMPLEMENTATION HERE*/
+    }
+
+
+    //main method for testing
+    public static void main( String[] args ) 
+    {
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	  Stack<String> tastyStack = new LLStack<String>();
+
+	  tastyStack.push("aoo");
+	  tastyStack.push("boo");
+	  tastyStack.push("coo");
+	  tastyStack.push("doo");
+	  tastyStack.push("eoo");
+	  tastyStack.push("foo");
+	  tastyStack.push("goo");
+	  tastyStack.push("hoo");
+	  tastyStack.push("ioo");
+	  tastyStack.push("joo");
+	  tastyStack.push("coocoo");
+	  tastyStack.push("cachoo");
+
+	  //cachoo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //coocoo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //joo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //ioo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //hoo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //goo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //foo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //eoo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //doo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //coo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //boo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  //aoo
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+
+	  //stack empty by now; SOP(null)
+	  System.out.println( "peek: " + tastyStack.peek() );
+	  System.out.println( "pop: " + tastyStack.pop() );
+	  System.out.println( tastyStack.pop() );
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }//end main()
+
+}//end class LLStack
